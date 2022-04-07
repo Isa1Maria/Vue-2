d3647a6b6f90f5365efe9ea482a2e6c5921db05a
Isabel Marques <50515191+Isa1Maria@users.noreply.github.com>
2022-04-07 19:17:23 +0200

    Initial commit

---
+++ src/vue-2.js
@@ -0,0 +1,21 @@
+import "./vue-2.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello coders!</h1>
+<div>
+  <p>
+    Welcome to your first code sandbox. Let's go on a tour. If you haven't already in the top right corner you can create an account or use your github account.
+  </p>
+  <p>
+    Now that you have an account, please make sure you are logged in and that you can click on fork in the right top corner. This duplicates the complete project, so that you can start working on it on your own.
+  </p>
+
+  <p>
+    Now that it's on your own environment, let's start changing some files! You can do that by just typing in the code view (in the middle). This will automatically update the preview view on the right. And on the left you can add, change or go to other files.
+  </p>
+
+  <p>
+    Another cool tip is: if you like some more space you can grab the URL on the top of your preview and use that in a different tab of your browser. Then you can close the preview screen here. 
+  </p>
+</div>
+`;
